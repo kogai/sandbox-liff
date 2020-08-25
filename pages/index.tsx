8b2e641af087e61adc0d071768110846adf7d22b
shinichi kogai <kogai0121@gmail.com>
2020-08-25 12:46:47 +0900

    see context

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Link from "next/link";
+// import liff from "@line/liff";
 import Layout from "../components/Layout";
 import getConfig from "next/config";
 
 const { publicRuntimeConfig } = getConfig();
 
 const IndexPage = () => {
+  const ctxRef = React.useRef(null);
   React.useEffect(() => {
-    // import liff from "@line/liff";
+    // console.log(liff.getContext());
     import("@line/liff").then((liff) => {
       console.log("process.browser", process.browser);
       // console.log(liff);
       console.log(publicRuntimeConfig);
+      // @ts-ignore
+      ctxRef.current = liff.getContext();
       // @ts-ignore
       liff.init({ liffId: publicRuntimeConfig.liffID });
     });
@@ -20,6 +24,7 @@ const IndexPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋</h1>
+      {JSON.stringify(ctxRef.current)}
       <p>
         <Link href="/about">
           <a>About</a>
